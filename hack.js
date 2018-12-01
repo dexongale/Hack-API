2371bb791fdd5ff2f011f7e010f4cbc845eb2477
jean72human <jsadovonon@gmail.com>
2018-12-01 07:03:51 +0000

    upload

--- hack.js
+++ hack.js
@@ -1,32 +1,15 @@
-function readURL(input) {
-  if (input.files && input.files[0]) {
-
-    var reader = new FileReader();
-
-    reader.onload = function(e) {
-      $('.image-upload-wrap').hide();
-
-      $('.file-upload-image').attr('src', e.target.result);
-      $('.file-upload-content').show();
-
-      $('.image-title').html(input.files[0].name);
-    };
-
-    reader.readAsDataURL(input.files[0]);
-
-  } else {
-    removeUpload();
-  }
+window.ajaxSuccess = function () {
+	response = JSON.parse(this.responseText);
+  console.log("ajaxSuccess", typeof this.responseText);
+  document.getElementById('uploaded').setAttribute("src", response["secure_url"]);
+  document.getElementById('results').innerText = this.responseText;
 }
 
-function removeUpload() {
-  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-  $('.file-upload-content').hide();
-  $('.image-upload-wrap').show();
+window.AJAXSubmit = function (formElement) {
+  console.log("starting AJAXSubmit");
+  if (!formElement.action) { return; }
+  var xhr = new XMLHttpRequest();
+  xhr.onload = ajaxSuccess;
+  xhr.open("post", "https://api.cloudinary.com/v1_1/dbl7whgry/image/upload");
+  xhr.send(new FormData(formElement));
 }
-$('.image-upload-wrap').bind('dragover', function () {
-        $('.image-upload-wrap').addClass('image-dropping');
-    });
-    $('.image-upload-wrap').bind('dragleave', function () {
-        $('.image-upload-wrap').removeClass('image-dropping');
-});
