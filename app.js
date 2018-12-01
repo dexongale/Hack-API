2b4acdee9cb32d1f918f62e8391d80d3a1655341
Derrick Osae <osae61@gmail.com>
2018-12-01 13:18:10 +0000

    refactored server

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var cors = require('cors')
 var logger = require("morgan");
 var express = require("express");
-var cookieParser = require("cookie-parser");
-
 var indexRouter = require("./routes/index");
 
 var app = express();
@@ -10,7 +8,6 @@ var app = express();
 app.use(cors())
 app.use(logger("dev"));
 app.use(express.json());
-app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
